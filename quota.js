2c7e8e245b7798e312ee4fbd00243129fd08fb1d
browsercapturesalt <browsercaptures@googlemail.com>
2022-01-18 14:21:35 +0000

    Server

--- quota.js
+++ quota.js
@@ -1,33 +1,43 @@
-const { exec } = require("child_process")
+const { exec } = require("child_process");
 
-function getOutput(command){
-    return new Promise(resolve=>{
-        exec(command, (err, stdout, stderr)=>{
-            resolve(stdout)
-        })
-    })
+function getOutput(command) {
+  return new Promise((resolve) => {
+    exec(command, (err, stdout, stderr) => {
+      resolve(stdout);
+    });
+  });
 }
 
-function quota(tokenName){
-    return new Promise(async resolve => {
-        process.env["HEROKU_API_KEY"] = process.env[tokenName]
-        const apps = await getOutput("heroku apps")
-        const firstApp = apps.split("\n")[1].split(" ")[0]
-        const q = await getOutput(`heroku ps -a ${firstApp}`)
-        const m = q.match(/Free dyno hours quota remaining this month: (.*)/)
-        if(m){
-            console.log(tokenName.split("_")[2].padEnd(25, " "), m[1])
-        }else{
-            console.log(tokenName, "could not get quota")
-        }
-        resolve()
-    })
+function quota(tokenName) {
+  return new Promise(async (resolve) => {
+    process.env["HEROKU_API_KEY"] = process.env[tokenName];
+    const apps = await getOutput("heroku apps");
+    const firstApp = apps.split("\n")[1].split(" ")[0];
+    const q = await getOutput(`heroku ps -a ${firstApp}`);
+    const m = q.match(/Free dyno hours quota remaining this month: (.*)/);
+    const name = tokenName.split("_")[2].padEnd(25, " ");
+    const log = m ? name + m[1] : name + "could not get quota";
+
+    resolve(log);
+  });
 }
 
-async function allQuotas(){
-    for(const key of Object.keys(process.env).filter(key => key.match(/^HEROKU_TOKEN/))){
-        await quota(key)
+function allQuotas() {
+  return new Promise(async (resolve) => {
+    let buff = "";
+    for (const key of Object.keys(process.env).filter((key) =>
+      key.match(/^HEROKU_TOKEN/)
+    )) {
+      const log = await quota(key);
+      console.log(log);
+      buff += log + "\n";
     }
+    resolve(buff);
+  });
 }
 
-allQuotas()
+allQuotas();
+
+module.exports = {
+  allQuotas,
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = parseInt(process.env.PORT || "3000");
+const path = require("path");
+
+app.use(express.json())
+
+const { allQuotas } = require("./quota");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/quota", async (req, res) => {
+  res.send(req.body.pass === process.env.ADMIN_PASS ? await allQuotas() : "Not Authorized");
+});
+
+app.listen(PORT, () => {
+  console.log(`Gp Heroku listening at ${PORT}`);
+});
